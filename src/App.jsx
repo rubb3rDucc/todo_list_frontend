01b015a377c36abb2e34021876d8c5140f1f6f39
rubb3rDucc <thomalin@oregonstate.edu>
2024-05-06 14:40:25 -0400

    add log in/ log out bar, does not function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css'
+import 'bulma/css/bulma.min.css';
 import Login from './components/Login/Login'
 import Dashboard from './components/Dashboard/Dashboard';
 import useToken from '/src/hooks/useToken';
+import Bar from './components/bar/bar';
 
 function App() {
   const {token, setToken} = useToken();
@@ -14,10 +16,12 @@ function App() {
   return (
     <>
       <div className="wrapper">
+        <Bar/>
         <h1>Application</h1>
         <BrowserRouter>
           <Routes>
             <Route path="/dashboard" element={<Dashboard />}/>
+
           </Routes>
         </BrowserRouter>
       </div>
